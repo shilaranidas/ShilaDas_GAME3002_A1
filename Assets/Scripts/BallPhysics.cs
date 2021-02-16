2f4d6ad227393b8d9f3d1a8da46622db84738620
shilaranidas <shilarani.das@georgebrown.ca>
2021-02-16 15:03:27 -0500

    add goal keeper

--- Assets/Scripts/BallPhysics.cs
+++ Assets/Scripts/BallPhysics.cs
@@ -1,10 +1,12 @@
 using UnityEngine.Assertions;
 using UnityEngine;
 using TMPro;
+using System.Collections;
 
 public class BallPhysics : MonoBehaviour
 {
     public TMP_Text scoreValuetxt;
+    public TMP_Text ballLefttxt;
     [SerializeField]
     private float m_fInputDeltaVal = 0.001f;
     [SerializeField]
@@ -16,6 +18,7 @@ public class BallPhysics : MonoBehaviour
     [SerializeField]
     private bool m_bDebugKickBall = false;
     public int score = 0;
+    public int BallLeft = 5;
     private Rigidbody m_rb = null;
     private GameObject m_TargetDisplay = null;
 
@@ -24,14 +27,14 @@ public class BallPhysics : MonoBehaviour
     private float m_fDistanceToTarget = 0f;
 
     private Vector3 vDebugHeading;
-    Vector3 startPosition;
+    //Vector3 startPosition;
     // Start is called before the first frame update
     void Start()
     {
         m_rb = GetComponent<Rigidbody>();
         Assert.IsNotNull(m_rb, "Houston, we've got a problem here! No Rigidbody attached");
-        startPosition= transform.position;
-        //CreateTargetDisplay();
+       // startPosition= transform.position;
+        CreateTargetDisplay();
 
     }
     private void OnCollisionEnter(Collision collision)
@@ -40,110 +43,124 @@ public class BallPhysics : MonoBehaviour
     }
     private void OnTriggerEnter(Collider other)
     {
-        m_rb.velocity = Vector3.zero;
-        m_rb.MovePosition(startPosition);
-        //if (other.gameObject.tag=="Goal")
-        //{
-        //    //transform.position = GameObject.Find("BallStartPosition").transform.position;
-        //    this.gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
-        //    //this.gameObject.GetComponent<Rigidbody>().velocity *= -1;
-        //    Debug.Log("goal");
-        //    score++;
-        //}
-        //if (other.gameObject.tag == "Boundary")
-        //{
-        //    //transform.position = GameObject.Find("BallStartPosition").transform.position;
-        //    //this.gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
-        //    this.gameObject.GetComponent<Rigidbody>().velocity *= -1;
-        //    Debug.Log("boundary");
-        //   // score++;
-        //}
-        //if (other.gameObject.tag == "Wall")
-        //{
-        //    //transform.position = GameObject.Find("BallStartPosition").transform.position;
-        //    //this.gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
-        //    this.gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
-        //    Debug.Log("wall");
-        //    // score++;
-        //}
-
+        //m_rb.velocity = Vector3.zero;
+        //m_rb.MovePosition(startPosition);
+        
+        if (other.gameObject.tag == "Boundary")
+        {
+            this.gameObject.GetComponent<Rigidbody>().velocity *= -1;
+            Debug.Log("boundary");     
+        }
+        else
+        {
+            if (other.gameObject.tag == "Goal")
+            {
+                Debug.Log("goal");
+                score++;
+            }
+            if (other.gameObject.tag == "Wall")
+            {
+                Debug.Log("wall");
+            }
+            if (other.gameObject.tag == "outofField")
+            {
+                Debug.Log("outfField");
+            }
+            transform.position = GameObject.Find("BallStartPosition").transform.position;
+            this.gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        }
+ 
     }
+   
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonUp(0))
-        {
+        //if (Input.GetMouseButtonUp(0))
+        //{
 
-            m_rb = GetComponent<Rigidbody>();
-            m_rb.AddRelativeForce(0, 100, 1000);
-        }
+        //   
+        //}
 
-        if (Input.GetMouseButtonUp(1))
-        {
-            m_rb.velocity = Vector3.zero;
-            m_rb.MovePosition(startPosition);
-        }
+        //if (Input.GetMouseButtonUp(1))
+        //{
+        //    m_rb.velocity = Vector3.zero;
+        //    m_rb.MovePosition(startPosition);
+        //}
 
-        //Rotate ball with mouse
-        float mouseX = (Input.mousePosition.x / Screen.width) - 0.5f;
-        float mouseY = (Input.mousePosition.y / Screen.height) - 0.5f;
-        transform.localRotation = Quaternion.Euler(new Vector4(-1f * (mouseY * 180f), mouseX * 360f, transform.localRotation.z));
-        // HandleUserInput();
+        ////Rotate ball with mouse
+        //float mouseX = (Input.mousePosition.x / Screen.width) - 0.5f;
+        //float mouseY = (Input.mousePosition.y / Screen.height) - 0.5f;
+        
+        //transform.localRotation = Quaternion.Euler(new Vector4(-1f * (mouseY * 180f), mouseX * 360f, transform.localRotation.z));
+       // Debug.Log("rot" + transform.localRotation);
+        HandleUserInput();
         //// GetLandingPosition();
-        // if (m_TargetDisplay != null && m_bIsGrounded)
-        // {
-        //     m_TargetDisplay.transform.position = m_vTargetPos;
-        //     Debug.Log("change" + m_TargetDisplay.transform.position);
-        //     vDebugHeading = m_vTargetPos - transform.position;
-        // }
+        if (m_TargetDisplay != null && m_bIsGrounded)
+        {
+            m_TargetDisplay.transform.position = m_vTargetPos;
+            Debug.Log("change" + m_TargetDisplay.transform.position+" "+m_vTargetPos);
+            vDebugHeading = m_vTargetPos - transform.position;
+        }
 
-        // if (m_bDebugKickBall && m_bIsGrounded)
-        // {
-        //     m_bDebugKickBall = false;
-        //     OnKickBall();
-        // }
-        // scoreValuetxt.text = score.ToString();
+        if (m_bDebugKickBall && m_bIsGrounded)
+        {
+            m_bDebugKickBall = false;
+            if (m_TargetDisplay != null && m_bIsGrounded)
+            {
+                m_TargetDisplay.transform.position = GetLandingPosition();
+            }
+            // OnKickBall();
+        }
+        scoreValuetxt.text = score.ToString();
+        ballLefttxt.text = BallLeft.ToString();
     }
-
+    public Material myM;
     private void CreateTargetDisplay()
     {
         m_TargetDisplay = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
-        m_TargetDisplay.transform.position = Vector3.zero;
-        m_TargetDisplay.transform.localScale = new Vector3(1.0f, 0.1f, 1.0f);
+        m_TargetDisplay.transform.position = new Vector3(0, .5f, 10);
+        m_TargetDisplay.GetComponent<MeshRenderer>().material = myM;
+        m_vTargetPos =new Vector3(0, .5f, 10);
+        
+        m_TargetDisplay.transform.localScale = new Vector3(.3f, 0.1f, .3f);
         m_TargetDisplay.transform.rotation = Quaternion.Euler(90f, 0f, 0f);
 
-        m_TargetDisplay.GetComponent<Renderer>().material.color = Color.red;
+        //m_TargetDisplay.GetComponent<Renderer>().material.color = Color.red;
         m_TargetDisplay.GetComponent<Collider>().enabled = false;
     }
 
-    public void OnKickBall()
-    {
-        // H = Vi^2 * sin^2(theta) / 2g
-        // R = 2Vi^2 * cos(theta) * sin(theta) / g
+    //public void OnKickBall()
+    //{
+    //    // H = Vi^2 * sin^2(theta) / 2g
+    //    // R = 2Vi^2 * cos(theta) * sin(theta) / g
 
-        // Vi = sqrt(2gh) / sin(tan^-1(4h/r))
-        // theta = tan^-1(4h/r)
+    //    // Vi = sqrt(2gh) / sin(tan^-1(4h/r))
+    //    // theta = tan^-1(4h/r)
 
-        // Vy = V * sin(theta)
-        // Vz = V * cos(theta)
-        Debug.Log("Kick " + m_TargetDisplay.transform.position);
-        m_fDistanceToTarget = (m_TargetDisplay.transform.position - transform.position).magnitude;
-        float fMaxHeight = m_TargetDisplay.transform.position.y;
-        float fRange = (m_fDistanceToTarget * 2);
-        float fTheta = Mathf.Atan((4 * fMaxHeight) / (fRange));
+    //    // Vy = V * sin(theta)
+    //    // Vz = V * cos(theta)
+    //    Debug.Log("Kick "+ m_TargetDisplay.transform.position+ " "+m_vTargetPos+" "+ transform.position);
+    //    //m_rb = GetComponent<Rigidbody>();
+    //    //m_rb.AddRelativeForce(0, 100, 100);
+    //    m_fDistanceToTarget = (m_TargetDisplay.transform.position - transform.position).magnitude;
+    //    float fMaxHeight = m_TargetDisplay.transform.position.y;
+    //    float fRange = (m_fDistanceToTarget * 2);
+    //    float fTheta = Mathf.Atan((4 * fMaxHeight) / (fRange));
 
-        float fInitVelMag = Mathf.Sqrt((2 * Mathf.Abs(Physics.gravity.y) * fMaxHeight)) / Mathf.Sin(fTheta);
+    //    float fInitVelMag = Mathf.Sqrt((2 * Mathf.Abs(Physics.gravity.y) * fMaxHeight)) / Mathf.Sin(fTheta);
 
-        m_vInitialVel.y = fInitVelMag * Mathf.Sin(fTheta);
-        m_vInitialVel.z = fInitVelMag * Mathf.Cos(fTheta);
+    //    m_vInitialVel.y = fInitVelMag * Mathf.Sin(fTheta);
+    //    m_vInitialVel.z = fInitVelMag * Mathf.Cos(fTheta);
 
-        m_rb.velocity = m_vInitialVel;
-    }
+    //    m_rb.velocity = m_vInitialVel;
+    //    BallLeft--;
+    //}
 
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
         Gizmos.DrawLine(transform.position + vDebugHeading, transform.position);
+        //Gizmos.DrawLine(transform.position,);
     }
     private Vector3 GetLandingPosition()
     {
@@ -155,12 +172,29 @@ public class BallPhysics : MonoBehaviour
 
         return m_vTargetPos + vFlatVel;
     }
+    public void OnLaunchProjectile()
+    {
+        if (!m_bIsGrounded)
+        {
+            return;
+        }
+
+        m_TargetDisplay.transform.position = GetLandingPosition();
+        m_bIsGrounded = false;
+
+        transform.LookAt(m_TargetDisplay.transform.position, Vector3.up);
+
+        m_rb.velocity = m_vInitialVelocity;
+    }
+
     private void HandleUserInput()
     {
         if (Input.GetKeyUp(KeyCode.Space))
         {
-            //m_projectile.OnLaunchProjectile();
+            //m_projectile.
             m_bDebugKickBall = true;
+            OnLaunchProjectile();
+           
         }
 
         if (Input.GetKey(KeyCode.W))
@@ -191,24 +225,19 @@ public class BallPhysics : MonoBehaviour
         {
             m_vTargetPos.y -= m_fInputDeltaVal;
         }
+
         if (Input.GetKey(KeyCode.UpArrow))
         {
-            
             m_rb.MovePosition(new Vector3(m_rb.transform.position.x , m_rb.transform.position.y, m_rb.transform.position.z + m_fInputDeltaVal));
         }
-
         if (Input.GetKey(KeyCode.DownArrow))
         {
-           
             m_rb.MovePosition(new Vector3(m_rb.transform.position.x , m_rb.transform.position.y, m_rb.transform.position.z - m_fInputDeltaVal));
         }
-
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            
             m_rb.MovePosition(new Vector3(m_rb.transform.position.x + m_fInputDeltaVal, m_rb.transform.position.y, m_rb.transform.position.z));
         }
-
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             m_rb.MovePosition(new Vector3(m_rb.transform.position.x - m_fInputDeltaVal, m_rb.transform.position.y, m_rb.transform.position.z));
---
+++ Assets/Scripts/GoalKeeperController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalKeeperController : MonoBehaviour
+{
+    float speed = 1f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        if (transform.position.x > 2.15)
+            speed = -1;
+        else if (transform.position.x < -2.35)
+            speed = 1;
+        transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
+    }
+}
